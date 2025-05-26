c04959fdc24a1f9fdfabb8c6f7b467cf59b7080d
idwin1 <112724428+idwin1@users.noreply.github.com>
2025-05-26 13:08:07 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
 $host = "10.10.0.4";  // IP privada del servidor MySQL
 $usuario = "idwin";  // Cambia esto por tu usuario de MySQL
-$contrasena = "Idwinuno1@";  // Cambia esto por tu contraseña de MySQL
+$contrasena = "Sandrauno1@";  // Cambia esto por tu contraseña de MySQL
 $base_datos = "datos";
 
 $conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
